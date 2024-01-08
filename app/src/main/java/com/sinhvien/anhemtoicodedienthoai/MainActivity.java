6ccd7cd0f0ca885eb3d941ba8459321d5d02fb97
Phan Xuân Thái <22dh113377@st.huflit.edu.vn>
2024-01-08 17:52:28 +0700

    .

--- app/src/main/java/com/sinhvien/anhemtoicodedienthoai/MainActivity.java
+++ app/src/main/java/com/sinhvien/anhemtoicodedienthoai/MainActivity.java
@@ -2,15 +2,27 @@ package com.sinhvien.anhemtoicodedienthoai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.sinhvien.anhemtoicodedienthoai.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+     ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        /*Issue here*/
+        /*binding.*/
+            
+        });
+
+
 
 
     }
