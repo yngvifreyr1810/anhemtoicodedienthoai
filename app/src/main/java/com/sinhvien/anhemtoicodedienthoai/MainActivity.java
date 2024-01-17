0d49eca632e2709512019e319c2863bded239ca5
Phan Xuân Thái <22dh113377@st.huflit.edu.vn>
2024-01-17 14:53:22 +0700

    Xong navbar

--- app/src/main/java/com/sinhvien/anhemtoicodedienthoai/MainActivity.java
+++ app/src/main/java/com/sinhvien/anhemtoicodedienthoai/MainActivity.java
@@ -1,29 +1,71 @@
 package com.sinhvien.anhemtoicodedienthoai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-import com.sinhvien.anhemtoicodedienthoai.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-     ActivityMainBinding binding;
-
+     FragmentManager fm = getSupportFragmentManager();
+     BottomNavigationView navBar;
+     FrameLayout frame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        /*Issue here*/
-        /*binding.*/
-            
+        setContentView(R.layout.activity_main);
+        loadFragment(fm,new HomeFragment());
+       frame = findViewById(R.id.frame_Layout);
+       navBar = findViewById(R.id.bottomNavigationView);
+       addEvent();
+
+    }
+    public void addEvent()
+    {
+        navBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id=item.getItemId();
+                if(id==R.id.home){
+                    loadFragment(fm,new HomeFragment());
+                    return true;
+                }
+                if(id==R.id.profile){
+                    loadFragment(fm,new ProfileFragment());
+                    return true;
+                }
+                if(id==R.id.bookmarks)
+                {
+                    loadFragment(fm,new BookmarksFragment());
+                    return true;
+                }
+                if(id==R.id.settings)
+                {
+                    loadFragment(fm,new SettingsFragment());
+                    return true;
+                }
+                if(id==R.id.search)
+                {
+                    loadFragment(fm,new SearchFragment());
+                    return true;
+                }
+                return false;
+            }
         });
+    }
 
-
-
-
+    public void loadFragment(FragmentManager fm, Fragment fragment) {
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame_Layout,fragment);
+        ft.commit();
     }
 }
